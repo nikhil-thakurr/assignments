e045c64f45df79cabaad3839cc59142ccf19461e
Nikhil Thakur <nt34542@gmail.com>
2024-10-08 07:49:15 +0530

    Easy changes

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,10 +1,27 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement user signup logic
+    const user = req.body ;
+    try {
+      const admin = new User({
+        username:user.username,
+        password:user.password
+      })
+
+      await admin.save();
+
+      res.json({
+        msg:"User created successfully"
+      })
+    }
+    catch(err){
+        res.status(400).send("ERROR : "+ err);
+    }
 });
 
 router.get('/courses', (req, res) => {
